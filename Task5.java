60181c6520031dda9f3409d38d65cf7dc1c2108d
karan2342 <karanshrestha544@gmail.com>
2025-06-26 16:13:54 +0545

    upload
    task

---
+++ Task5.java
@@ -0,0 +1,25 @@
+/*
+ *create a class student having id, name, program as field , with a default constructor and two setter named with setstudent, one setter have two parameter to set id and name . Another setter have 3 parameter id, name and program. also create a display method to display all info use the concept of method overloading
+ */
+
+
+class students{
+ int id;
+ String name;
+ String program;
+
+ students(){
+    id = 0;
+    name = null;
+    program = null;
+ }
+}
+
+
+
+
+
+
+public class Task5 {
+    
+}
